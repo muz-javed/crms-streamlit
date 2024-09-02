3d0cd9ce41c16f1f912d6b9de24369383ae7e298
muz-javed <muzammil.jvd@outlook.com>
2024-09-03 03:31:21 +0500

    Update crms.py

--- crms.py
+++ crms.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 from datetime import datetime
-import langchain
+# import langchain
 
 from openai_functions import bankruptcy_status
 
