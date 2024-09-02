c02017c2583bc91631dc35fa3b7900e04833b27c
muz-javed <muzammil.jvd@outlook.com>
2024-09-03 03:46:16 +0500

    Update crms.py

--- crms.py
+++ crms.py
@@ -9,7 +9,7 @@ import openai
 
 st.markdown(f"""<div style="border-radius: 5px;"><h3 style="text-align:left; color: black; font-weight: bold;">Credit Risk Management Standards</h3></div>""", unsafe_allow_html=True)
 
-upload_raw_file = st.file_uploader('Upload Data Files', type = ['xlsx'])
+upload_raw_file = st.file_uploader('Upload Data Files', type = 'xlsx')
 
 if upload_raw_file:
   raw_file = pd.read_excel(upload_raw_file).head(5)
