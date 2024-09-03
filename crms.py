a1ba8e709e996ece8184bfbcba940f7940ab207f
muz-javed <muzammil.jvd@outlook.com>
2024-09-03 23:20:47 +0500

    Update crms.py

--- crms.py
+++ crms.py
@@ -25,6 +25,7 @@ if upload_raw_file:
 
 
   ### CONVERT TO THE POWER-BI FORMAT ###
+  # UTP - RAW file #
   raw_file.insert(0, 'added_at', datetime.now())
 
   raw_file['default_trigger'] = raw_file[raw_file.columns[6:]].sum(axis = 1)
@@ -35,12 +36,25 @@ if upload_raw_file:
   cols.insert(line_of_business_index + 1, cols.pop(cols.index("default_trigger")))
   raw_file = raw_file[cols]
 
-  dataset_id = 'crms_dataset'
-  table_id = 'utp_raw'
-  df = raw_file.sort_values('customer_id')
+  # TRANSFORMED FILE #
+  trigger_cols = raw_file.columns[6:]
+  final_df = pd.DataFrame(columns = ['customer_id', 'customer_name', 'asset_type', 'line_of_business', 'exposure_amount', 'trigger', 'flag', 'default_trigger'])
+  
+  for i in range(0, len(raw_file)):
+    for j in trigger_cols:
+  
+      temp_df_list = [raw_file['customer_id'][i], raw_file['customer_name'][i], raw_file['asset_type'][i], raw_file['line_of_business'][i], raw_file['exposure_amount'][i], j, raw_file[j][i], raw_file['default_trigger'][i]]
+      temp_df = pd.DataFrame(temp_df_list).T
+      temp_df.columns = final_df.columns
+
+      final_df = pd.concat([final_df, temp_df]).reset_index(drop = True)
+  
+  final_df['cust_def_flag'] = 'No'
+  final_df.loc[final_df['default_trigger'] == 1, 'cust_def_flag'] = 'Yes'
   
   with st.spinner('Data is being loaded...'):
-    load_df_to_bq(df, dataset_id, table_id)
+    load_df_to_bq(raw_file.sort_values('customer_id'), 'crms_dataset', 'utp_raw')
+    load_df_to_bq(final_df, 'crms_dataset', 'utp_transformed')
   
   st.markdown("""<div style='text-align: left; padding-left: 10px; color: green; border-radius: 5px;'><p>Data has been loaded successfully.</p></div>""", unsafe_allow_html=True)
 
