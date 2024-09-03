8ed9f14dd9b539a0a07e5b8213949792d054d972
muz-javed <muzammil.jvd@outlook.com>
2024-09-03 05:50:30 +0500

    Update crms.py

--- crms.py
+++ crms.py
@@ -34,3 +34,41 @@ if upload_raw_file:
 
   st.write(raw_file)
 
+
+
+
+
+
+  from google.cloud import bigquery
+  from google.oauth2 import service_account
+  import pandas as pd
+  
+  # Path to your service account key file (JSON format)
+  key_path = "gen-lang-client-0773467639-eb3bb34e9803.json"
+  
+  # Credentials from the service account key file
+  credentials = service_account.Credentials.from_service_account_file(key_path)
+  
+  # Initialize the BigQuery client with the credentials
+  client = bigquery.Client(credentials=credentials, project=credentials.project_id)
+  
+  # Define your dataset and table name
+  dataset_id = 'crms_dataset'
+  table_id = 'utp_raw'
+  table_full_id = f"{client.project}.{dataset_id}.{table_id}"
+  
+  # Define the job configuration with schema autodetection
+  job_config = bigquery.LoadJobConfig(
+      autodetect=True,
+      write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE  # This overwrites the table if it already exists
+  )
+  
+  # Load the DataFrame into the BigQuery table
+  load_job = client.load_table_from_dataframe(raw_file, table_full_id, job_config=job_config)  # Make an API request.
+  
+  load_job.result()  # Wait for the job to complete.
+  
+  st.write(f"Loaded {load_job.output_rows} rows into {table_full_id}.")
+
+
+
