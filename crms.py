18208446d2655a44e7e718d26e485b3aa08794fd
muz-javed <muzammil.jvd@outlook.com>
2024-09-03 23:11:04 +0500

    Update crms.py

--- crms.py
+++ crms.py
@@ -7,6 +7,7 @@ from datetime import datetime
 from google.cloud import bigquery
 from google.oauth2 import service_account
 from openai_functions import bankruptcy_status
+from dataframe_functions import load_df_to_bq
 
 st.markdown(f"""<div style="border-radius: 5px;"><h3 style="text-align:left; color: black; font-weight: bold;">Credit Risk Management Standards</h3></div>""", unsafe_allow_html=True)
 
@@ -45,33 +46,10 @@ dataset_id = 'crms_dataset'
 table_id = 'utp_raw'
 df = raw_file.sort_values('customer_id')
 
-def load_df_to_bq(df, dataset_id, table_id)
-  # Path to your service account key file (JSON format)
-  key_path = "gen-lang-client-0773467639-eb3bb34e9803.json"
-  
-  # Credentials from the service account key file
-  credentials = service_account.Credentials.from_service_account_file(key_path)
-  
-  # Initialize the BigQuery client with the credentials
-  client = bigquery.Client(credentials=credentials, project=credentials.project_id)
-  
-  # Define your dataset and table name
-  table_full_id = f"{client.project}.{dataset_id}.{table_id}"
-  
-  # Define the job configuration with schema autodetection
-  job_config = bigquery.LoadJobConfig(
-      autodetect=True,
-      write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE  # This overwrites the table if it already exists
-  )
-  
-  # Load the DataFrame into the BigQuery table
-  load_job = client.load_table_from_dataframe(df, table_full_id, job_config=job_config)  # Make an API request.
+with st.spinner('Data is being loaded...'):
+  load_df_to_bq(df, dataset_id, table_id)
 
-  with st.spinner('Data is being loaded...'):
-    load_job.result()  # Wait for the job to complete.
-    
-    st.markdown("""<div style='text-align: left; padding-left: 10px; color: green; border-radius: 5px;'><p>Data has been loaded successfully.</p></div>""", unsafe_allow_html=True)
-  
+st.markdown("""<div style='text-align: left; padding-left: 10px; color: green; border-radius: 5px;'><p>Data has been loaded successfully.</p></div>""", unsafe_allow_html=True)
 
 
 
