3a2b8d9cb0b9c59897da82e370095504e0459701
muz-javed <muzammil.jvd@outlook.com>
2024-09-04 00:46:47 +0500

    Update crms.py

--- crms.py
+++ crms.py
@@ -9,6 +9,9 @@ from google.oauth2 import service_account
 from openai_functions import bankruptcy_status
 from dataframe_functions import load_df_to_bq
 
+with st.side_bar:
+  st.write()
+
 st.markdown(f"""<div style="border-radius: 5px;"><h3 style="text-align:left; color: black; font-weight: bold;">Credit Risk Management Standards</h3></div>""", unsafe_allow_html=True)
 
 upload_raw_file = st.file_uploader('Upload Data Files', type = 'xlsx')
