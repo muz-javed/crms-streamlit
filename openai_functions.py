f70a7bb8f03135bf573677e87fa26f271d4e5fb1
muz-javed <muzammil.jvd@outlook.com>
2024-09-06 05:09:07 +0500

    Update openai_functions.py

--- openai_functions.py
+++ openai_functions.py
@@ -16,7 +16,8 @@ import streamlit as st
 
 import os
 from langchain_google_community import GoogleSearchAPIWrapper
-from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_openai import OpenAIEmbeddings
+from langchain_openai import ChatOpenAI
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnablePassthrough
