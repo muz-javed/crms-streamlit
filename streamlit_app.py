4fbdc2806c17792d8b59da530ea4729a6d3d4db5
muz-javed <muzammil.jvd@outlook.com>
2024-09-06 08:10:08 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ from io import BytesIO
 from datetime import datetime
 from google.cloud import bigquery 
 from google.oauth2 import service_account
-# from openai_functions import external_bankruptcy_status
+from openai_functions import external_bankruptcy_status
 # from dataframe_functions import load_df_to_bq
 
 
